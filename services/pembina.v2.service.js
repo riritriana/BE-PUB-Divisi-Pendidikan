99056192377aaadfdcc279d51c698032d28efe72
Ali Hanafiah <hanafiaha308@gmail.com>
2024-04-15 22:55:50 +0700

    tambah validasi add pelatihan

--- services/pembina.v2.service.js
+++ services/pembina.v2.service.js
@@ -8,19 +8,26 @@ const AddPelatihan = async (req) => {
     let data = [];
     if (req.account.role === "pembina") {
         const { name, id_categori_pelatihan, hari, jam, id_pelatihan_instruktur } = req.body;
+        console.log(req.body);
         const check = await Pelatihan.findAll({ where: { name, id_categori_pelatihan } });
         if (check.length > 0) {
             msg = "Nama Kelompok sudah di pakek";
         } else {
             const id_jadwal = await JadwalPelatihan.findJamHari(jam, hari);
-            data = await Pelatihan.create({
-                name,
-                id_categori_pelatihan,
-                id_pelatihan_user: id_pelatihan_instruktur,
-                id_jadwal: id_jadwal.id,
-            });
-            msg = "berhasil hore";
-            status = true;
+            if (id_jadwal) {
+                data = await Pelatihan.create({
+                    name,
+                    id_categori_pelatihan,
+                    id_pelatihan_user: id_pelatihan_instruktur,
+                    id_jadwal: id_jadwal.id,
+                });
+
+                msg = "berhasil hore";
+                status = true;
+            }else {
+                msg = "jadwal Tidak Ditemukan"
+            }
+            
         }
 
     }
